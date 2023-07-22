fc8c91ed67e98b4947928862107e424b22882cd9
Victoria Escalada <viqiescalada@gmail.com>
2023-07-22 10:43:42 -0300

    iniciando proyecto

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import { useRoutes } from 'react-router-dom' 
+import HomeImage from './HomeImage'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <HomeImage/>
+    </div>
+  )
+}
